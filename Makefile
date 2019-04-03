57528df0c80cc714d1b78221fdb5a5a476985e79
rope1505 <rpelkner@googlemail.com>
2019-04-03 18:38:37 +0200

    Erster Graph

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ all: build/main.pdf
 # hier Python-Skripte:
 build/plot.pdf: plot.py matplotlibrc header-matplotlib.tex | build
 	TEXINPUTS=$$(pwd): python plot.py
+build/raumtemp.pdf: raumtemp.py matplotlibrc header-matplotlib.tex | build
+	TEXINPUTS=$$(pwd): python raumtemp.py
 
 # hier weitere Abhängigkeiten für build/main.pdf deklarieren:
 build/main.pdf: build/plot.pdf
---
+++ raumtemp.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+def csv_read(pathToFile, delimiter=";"):
+    with open(pathToFile, "r") as f:
+        content = []
+        for line in f:
+            content.append((line.rstrip()).split(delimiter))
+    return content
+
+def func(x, a, b):
+    return a*x +b
+
+
+werte = csv_read("csv/raumtemp.csv")
+xdata = np.zeros(29)
+ydata = np.zeros(29)
+ignore = True
+i=0
+
+
+
+for values in werte:
+    if(i >= 29):
+        break
+    if(ignore):
+        ignore = False
+    else:
+        xdata[i] = float(values[0])
+        ydata[i] = float(values[1])
+        
+        i+=1
+
+
+x_line = np.linspace(0, 10.2)
+
+
+plt.plot(xdata, ydata, 'r.', label="Energieverteilung")
+#popt1, pcov1 = curve_fit(func, xdata, ydata)
+#plt.plot(x_line, func(x_line, *popt1), "r-", label="Fit")
+plt.xlabel(r"$U_\text{A}$ / $\symup{V}$")
+plt.ylabel(r"Steigung")
+#print(popt1)
+#print(np.sqrt(np.diag(pcov1)))
+plt.legend()
+plt.tight_layout()
+plt.savefig("build/raumtemp.pdf")
